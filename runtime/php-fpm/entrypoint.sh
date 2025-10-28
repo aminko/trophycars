953255216b6b9c52ec4acde697a8a7ee06ca463b
aminko <andzej.minko@gmail.com>
2025-10-28 10:46:41 +0100

    Updating dependencies

--- runtime/php-fpm/entrypoint.sh
+++ runtime/php-fpm/entrypoint.sh
@@ -9,6 +9,12 @@ GROUP_ID=${GID:-1000}
 echo "Fixing file permissions with UID=${USER_ID} and GID=${GROUP_ID}..."
 chown -R ${USER_ID}:${GROUP_ID} /srv || echo "Some files could not be changed"
 
+# In case missing autoload
+if [ ! -f /srv/vendor/autoload.php ]; then
+    echo "Generating autoload file..."
+    composer install --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
+fi
+
 # Clear configurations to avoid caching issues in development
 echo "Clearing configurations..."
 php artisan config:clear
